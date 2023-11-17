b491203fbc9b6b9a04698b6eee1344dc6abe068b
BretShep03 <bretshep03@gmail.com>
2023-11-17 15:06:40 -0600

    Update index.js
    Added comments to Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// Wait for the DOM to be ready before executing the code
 $(document).ready(function () {
+    // Get references to DOM elements and initialize variables
     const taskList = document.getElementById("tasksList");
     const taskNameInput = document.getElementById("taskName");
     const tasks = [];
@@ -9,9 +11,10 @@ $(document).ready(function () {
     var canClick = true;
     var clickTimeout;
 
-    // Class(es)
+    // Class definition for Task
     class Task {
         constructor(elem, desc) {
+            // Validate input parameters
             if (typeof desc !== 'string') {
                 throw new Error("Task description must be a string.");
             } else if ($(elem).get(0).tagName !== "LI") {
@@ -21,28 +24,33 @@ $(document).ready(function () {
         }
     }
 
-    // Event Listeners
+    // Event Listeners setup
     $("#addTaskBtn").on("click", addNewTask);
     $("#hideDescBtn").on("click", hideDesc);
     $("#deleteBtn").on("click", removeSelectedTask);
 
+    // Event listener for input changes in the task description textarea
     $('#taskDesc').find('form').find('textarea').on("input", function () {
         tasks[$(selectedTaskElem).index()].desc = $('#taskDesc').find('form').find('textarea').val();
     });
 
-    $("#taskDesc").hide()
+    // Hide the task description initially
+    $("#taskDesc").hide();
 
-    // Functions
+    // Function to check if the task name input is empty
     function isTaskNameInputEmpty() {
         return taskNameInput.value.trim() === "";
     }
 
+    // Function to add a new task to the list
     function addNewTask() {
         if (!isTaskNameInputEmpty()) {
             const li = document.createElement("li");
 
+            // Set the text content of the new task
             li.textContent = taskNameInput.value;
 
+            // Add event listeners for mouse interactions on the new task
             $(li).on("mouseenter", function () {
                 highlightTask(li);
             });
@@ -61,15 +69,20 @@ $(document).ready(function () {
                 handleTaskFocusOut(li);
             });
 
+            // Append the new task to the task list
             taskList.appendChild(li);
+
+            // Create a new Task object and add it to the tasks array
             tasks.push(new Task(li, ""));
         }
     }
 
+    // Function to hide the task description
     function hideDesc() {
         $('#taskDesc').hide(descAnimTime);
     }
 
+    // Function to remove the selected task
     function removeSelectedTask() {
         if (selectedTaskElem !== null) {
             const _selectedTaskElem = selectedTaskElem;
@@ -79,24 +92,29 @@ $(document).ready(function () {
         }   
     }
 
+    // Function to highlight the task text
     function highlightTask(elem) {
         if (elem !== null) { elem.style.color = "blue"; }
     }
 
+    // Function to reset the task text color
     function resetTaskTextColor(elem) {
         if (elem !== null) { elem.style.color = "black"; }
     }
 
+    // Function to update the task description text
     function updateDescText() {
         $('#taskDesc').find('form').find('textarea').val(tasks[$(selectedTaskElem).index()].desc);
     }
 
+    // Function to show the task description
     function showDesc() {
         updateDescText();
         $('#taskDesc').show(descAnimTime);
     }
      
     // Input Handler Functions
+    // Function to handle a single click on a task
     function handleTaskClick(taskElem) {
         clearTimeout(clickTimeout);
         clickTimeout = setTimeout(function () {
@@ -119,6 +137,7 @@ $(document).ready(function () {
         }, inputDelayTime);
     }
 
+    // Function to handle a double click on a task
     function handleTaskDblClick(taskElem) {
         clearTimeout(clickTimeout);
         $(taskElem).attr('contenteditable', "true")
@@ -126,6 +145,7 @@ $(document).ready(function () {
         $(taskElem).off('click');
     }
 
+    // Function to handle focus out event on a task
     function handleTaskFocusOut(taskElem) {
         $(taskElem).attr('contenteditable', "false")
         $(taskElem).on("click", function () {
